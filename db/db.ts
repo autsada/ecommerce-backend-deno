9b8215ce4083a04fba45a8609a13cfef0a977b66
autsada <autsada.t@gmail.com>
2021-06-24 09:38:18 +0700

    Change database connection to use heroku-postgresql

--- db/db.ts
+++ db/db.ts
@@ -1,23 +1,14 @@
 import { Pool, PoolClient, config } from '../deps.ts'
 
-const { DB_DATABASE, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = config()
+const { DATABASE_URL } = config()
 
 const POOL_CONNECTIONS = 20
-const pool = new Pool(
-  {
-    database: DB_DATABASE,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    hostname: DB_HOST,
-    port: +DB_PORT,
-  },
-  POOL_CONNECTIONS
-)
+const pool = new Pool(DATABASE_URL, POOL_CONNECTIONS, true)
 
 export async function runQuery<T extends {}>(query: string) {
-  const client: PoolClient = await pool.connect()
-  const result = await client.queryObject<T>(query)
-  client.release()
+    const client: PoolClient = await pool.connect()
+    const result = await client.queryObject<T>(query)
+    client.release()
 
-  return result
+    return result
 }
